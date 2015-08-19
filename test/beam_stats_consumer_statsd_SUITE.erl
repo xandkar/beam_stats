06cf34fbf4a707198201622f97ac86d7f1bb12c6
Siraaj Khandkar <siraaj@khandkar.net>
2015-08-19 10:58:56 -0400

    Use more-standard test case name prefixes.

--- test/beam_stats_consumer_statsd_SUITE.erl
+++ test/beam_stats_consumer_statsd_SUITE.erl
@@ -7,11 +7,11 @@
 
 %% Test cases
 -export(
-    [ ct_test__beam_stats_to_bins/1
-    , ct_test__memory_component_to_statsd_msg/1
-    , ct_test__statsd_msg_add_name_prefix/1
-    , ct_test__statsd_msg_to_bin/1
-    , ct_test__node_id_to_bin/1
+    [ t_beam_stats_to_bins/1
+    , t_memory_component_to_statsd_msg/1
+    , t_statsd_msg_add_name_prefix/1
+    , t_statsd_msg_to_bin/1
+    , t_node_id_to_bin/1
     ]).
 
 -define(statsd_module, beam_stats_consumer_statsd).
@@ -26,11 +26,11 @@ all() ->
 
 groups() ->
     Tests =
-        [ ct_test__beam_stats_to_bins
-        , ct_test__memory_component_to_statsd_msg
-        , ct_test__statsd_msg_add_name_prefix
-        , ct_test__statsd_msg_to_bin
-        , ct_test__node_id_to_bin
+        [ t_beam_stats_to_bins
+        , t_memory_component_to_statsd_msg
+        , t_statsd_msg_add_name_prefix
+        , t_statsd_msg_to_bin
+        , t_node_id_to_bin
         ],
     Properties = [],
     [{?GROUP, Properties, Tests}].
@@ -39,17 +39,17 @@ groups() ->
 %%  Test cases
 %% =============================================================================
 
-ct_test__beam_stats_to_bins(_Cfg) ->
+t_beam_stats_to_bins(_Cfg) ->
     ?statsd_module:ct_test__beam_stats_to_bins(_Cfg).
 
-ct_test__memory_component_to_statsd_msg(_Cfg) ->
+t_memory_component_to_statsd_msg(_Cfg) ->
     ?statsd_module:ct_test__memory_component_to_statsd_msg(_Cfg).
 
-ct_test__statsd_msg_add_name_prefix(_Cfg) ->
+t_statsd_msg_add_name_prefix(_Cfg) ->
     ?statsd_module:ct_test__statsd_msg_add_name_prefix(_Cfg).
 
-ct_test__statsd_msg_to_bin(_Cfg) ->
+t_statsd_msg_to_bin(_Cfg) ->
     ?statsd_module:ct_test__statsd_msg_to_bin(_Cfg).
 
-ct_test__node_id_to_bin(_Cfg) ->
+t_node_id_to_bin(_Cfg) ->
     ?statsd_module:ct_test__node_id_to_bin(_Cfg).
