5acc9b7d2c0966e92e539f9906ae9f0786ae3389
Siraaj Khandkar <siraaj@khandkar.net>
2015-09-16 17:58:26 -0400

    Remove default processes count totals.

--- include/beam_stats_processes.hrl
+++ include/beam_stats_processes.hrl
@@ -1,11 +1,11 @@
 -record(beam_stats_processes,
-    { individual_stats         = [] :: [beam_stats_process:t()]
-    , count_all                = 0  :: non_neg_integer()
-    , count_exiting            = 0  :: non_neg_integer()
-    , count_garbage_collecting = 0  :: non_neg_integer()
-    , count_registered         = 0  :: non_neg_integer()
-    , count_runnable           = 0  :: non_neg_integer()
-    , count_running            = 0  :: non_neg_integer()
-    , count_suspended          = 0  :: non_neg_integer()
-    , count_waiting            = 0  :: non_neg_integer()
+    { individual_stats         :: [beam_stats_process:t()]
+    , count_all                :: non_neg_integer()
+    , count_exiting            :: non_neg_integer()
+    , count_garbage_collecting :: non_neg_integer()
+    , count_registered         :: non_neg_integer()
+    , count_runnable           :: non_neg_integer()
+    , count_running            :: non_neg_integer()
+    , count_suspended          :: non_neg_integer()
+    , count_waiting            :: non_neg_integer()
     }).
